59e1217b800f96ef5da202a69eb584d913ef9e28
nindyasaff <63599760+nindyasaff@users.noreply.github.com>
2021-12-07 07:55:46 +0700

    Add files via upload

---
+++ deseria-serialization/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_Deserialization
+{
+    [Serializable]
+    class Tutorial
+    {
+        public int ID_Player;
+        public String Name;
+
+        static void Main(string[] args)
+        {
+            Tutorial obj = new Tutorial();
+            obj.ID_Player = 1;
+            obj.Name = "successs";
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(@"C:\Users\NINDYASAFF\Documents\deserialization-serialization\deseria-serialization\DataNama.txt", FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, obj);
+            stream.Close();
+
+            stream = new FileStream(@"C:\Users\NINDYASAFF\Documents\deserialization-serialization\deseria-serialization\DataNama.txt", FileMode.Open, FileAccess.Read);
+            Tutorial objnew = (Tutorial)formatter.Deserialize(stream);
+
+            Console.WriteLine(objnew.ID_Player);
+            Console.WriteLine(objnew.Name);
+
+            Console.ReadKey();
+        }
+    }
+}
